65b14721519835693f2bf5abbde327ec50707e8f
Kadir Kobak <kadir.kobak@celsan.com>
2025-04-16 09:42:28 +0300

    Db configuration.

---
+++ FilmAndSeries_CodeFirst/DAL/Context/MovieContext.cs
@@ -0,0 +1,18 @@
+﻿using FilmAndSeries_CodeFirst.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilmAndSeries_CodeFirst.DAL.Context
+{
+    public class MovieContext: DbContext
+    {
+
+        public DbSet<Movie> Movies { get; set; }
+
+
+    }
+}
---
+++ FilmAndSeries_CodeFirst/DAL/Entities/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilmAndSeries_CodeFirst.DAL.Entities
+{
+    public class Movie
+    {
+        public int MovieId { get; set; }
+        public string MovieTitle { get; set; }
+        public string MovieCategory { get; set; }
+        public int MovieDuration { get; set; } 
+        public string MovieDescription { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
